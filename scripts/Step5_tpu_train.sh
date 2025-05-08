23b69dcd330ae7285d19260265ae44c98acbc1bc
Bram van Es <bramiozo@gmail.com>
2025-05-08 21:11:28 +0200

    fixed sh train

--- scripts/Step5_tpu_train.sh
+++ scripts/Step5_tpu_train.sh
@@ -1,41 +1,43 @@
 #!/bin/bash
-#set -o allexport
-#source ../.env
-#set +o allexport
+set -o allexport
+source ../.env
+set +o allexport
 
-echo "Setting environment variables..."
+echo "Setting environment variables... (TPU_NAME=${TPU_NAME}, ZONE=${ZONE}, PROJECT_ID=${PROJECT_ID})"
 gcloud compute tpus tpu-vm ssh ${TPU_NAME} \
     --zone=${ZONE} \
     --project=${PROJECT_ID} \
     --worker=all \
     --command="export PJRT_DEVICE=TPU && export XLA_USE_PJRT=1 && export TPU_NAME=${TPU_NAME} && export OMP_NUM_THREADS=1 && export HF_DATASETS_VERBOSITY=debug && export TOKENIZERS_PARALLELISM=false"
 
-echo "Removing cache folders and logs..."
+echo "Removing cache folders and logs... (TPU_NAME=${TPU_NAME}, ZONE=${ZONE}, PROJECT_ID=${PROJECT_ID}, USERNAME=${USERNAME})"
 gcloud compute tpus tpu-vm ssh ${TPU_NAME} \
     --zone=${ZONE} \
     --project=${PROJECT_ID} \
     --worker=all \
     --command="rm -rf /home/${USERNAME}/.cache/*; rm ~/logs.txt"
 
-echo "Uploading .env and training wrapper script..."
+echo "Uploading .env and training wrapper script... (TPU_NAME=${TPU_NAME}, ZONE=${ZONE}, PROJECT_ID=${PROJECT_ID}, USERNAME=${USERNAME})"
 gcloud compute tpus tpu-vm scp ../.env train_wrapper.sh ${TPU_NAME}:/home/${USERNAME}/ \
-    --zone=${ZONE} --project=${PROJECT_ID} --worker=all
+    --zone=${ZONE} \
+    --project=${PROJECT_ID} \
+    --worker=all
 
-echo "Stopping all running processes..."
+echo "Stopping all running processes... (TPU_NAME=${TPU_NAME}, ZONE=${ZONE}, PROJECT_ID=${PROJECT_ID})"
 gcloud compute tpus tpu-vm ssh ${TPU_NAME} \
     --zone=${ZONE} \
     --project=${PROJECT_ID} \
     --worker=all \
     --command="sudo pkill -f python3"
 
-echo "Killing previous persistent training session..."
+echo "Killing previous persistent training session... (TPU_NAME=${TPU_NAME}, ZONE=${ZONE}, PROJECT_ID=${PROJECT_ID})"
 gcloud compute tpus tpu-vm ssh ${TPU_NAME} \
     --zone=${ZONE} \
     --project=${PROJECT_ID} \
     --worker=all \
     --command="tmux kill-session -t train_session"
 
-echo "Launching persistent training session..."
+echo "Launching persistent training session... (TPU_NAME=${TPU_NAME}, ZONE=${ZONE}, PROJECT_ID=${PROJECT_ID})"
 gcloud compute tpus tpu-vm ssh ${TPU_NAME} \
   --zone=${ZONE} \
   --project=${PROJECT_ID} \
@@ -43,6 +45,8 @@ gcloud compute tpus tpu-vm ssh ${TPU_NAME} \
   --command="chmod +x ~/train_wrapper.sh && tmux new-session -d -s train_session '~/train_wrapper.sh'"
 
 gcloud compute tpus tpu-vm ssh ${TPU_NAME} \
+    --zone=${ZONE} \
+    --project=${PROJECT_ID} \
     --worker=all \
     --command="tmux ls"
 
